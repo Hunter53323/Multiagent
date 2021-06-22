60cf10077abbac145135fa7b748b716515d94858
Hunter53323 <953973860@qq.com>
2021-06-22 10:14:13 +0800

    电池单智能体结构

---
+++ Env.py
@@ -0,0 +1,25 @@
+"能源系统多智能体强化学习环境--单电池"
+
+import math
+import gym
+from gym import spaces, logger
+from gym.utils import seeding
+import numpy as np
+
+
+class Multiagent_Battery(gym.Env):
+
+    def __init__(self):
+        pass
+
+    def step(self,action):
+        pass
+
+    def reset(self):
+        pass
+    
+    def render(self):
+        pass
+
+    def close(self):
+        pass
