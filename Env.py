e5682a74a8810637b0333c843c2dbf06e7507523
Hunter53323 <953973860@qq.com>
2021-06-22 15:12:03 +0800

    电池智能体

--- Env.py
+++ Env.py
@@ -5,13 +5,21 @@ import gym
 from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
+from Agent import Battery
 
 
-class Multiagent_Battery(gym.Env):
+class Multiagent_energy(gym.Env):
 
-    def __init__(self):
-        pass
+    def __init__(self,mode = "test"):
+        self.battery_electricity = 0
+        self.electricity_price_all = [0.3,0.3,0.3,0.3,0.3,0.3,0.3,1,1,1,1,0.6,0.6,0.6,0.6,0.6,1,1,1,1,0.6,0.6,0.6,0.3]
+        self.gas_price_all = 0.3
 
+        self.agent = []  #暂定用list实现
+        if mode == "test":
+            Agent = Battery()
+            self.agent.append(Agent)
+        
     def step(self,action):
         pass
 
