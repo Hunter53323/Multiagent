b710d37d7c31303b4fda1d83878dee11f7e9c7f6
Hunter53323 <953973860@qq.com>
2021-06-24 13:29:18 +0800

    参数定义

---
+++ defination.py
@@ -0,0 +1,14 @@
+import numpy as np
+OBSERVATION = np.array(['battery_electricity', 
+                'current_electricity_price', 
+                'current_gas_price',
+                'electricity_demand',
+                'gas_demand',
+                'heat_demand'])
+AGENT = ['battery','user']
+obs_battery = [0,1,3]
+obs_other = []
+
+OBSERVATION_BATTERY = OBSERVATION[obs_battery]
+OBSERVATION_OTHER = []
+
